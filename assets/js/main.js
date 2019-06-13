d48c1380c81b222b24795f019c657aec02f6830a
Beiker <beiker19998@gmail.com>
2019-06-13 11:30:19 -0400

    Descipción de equipos

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+var modal = document.getElementByI('miModal');
+let flex-modal = document.getElementById('flex-modal');
+var abrir = document.getElementById('abrir');
+let cerrar = document.getElementById('close');
+
+
+abrir.addEventListener('click',function(){
+  modal.style.display = 'block';
+});
